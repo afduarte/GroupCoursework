791f449d9da608e1db9a7879244e6dc40de46632
Antero Duarte <mail@afduarte.ovh>
2015-12-07 22:53:38 +0000

    Created RPS

--- rps/src/Servlet.java
+++ rps/src/Servlet.java
@@ -1,20 +1,14 @@
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Created by antero on 07/12/15.
- */
-@WebServlet(name = "Servlet")
+
+
 public class Servlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
